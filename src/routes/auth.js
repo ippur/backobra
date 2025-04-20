f29164c9880e4ef56b530e1c051ef60c628201ac
HabeckSystem <fhabeck@gmail.com>
2025-04-20 12:44:37 -0300

    fix: troca bcrypt por bcryptjs para compatibilidade com ESM no Render

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 // src/routes/auth.js
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import pool from '../../db/client.js';
 
